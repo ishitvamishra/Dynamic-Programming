e628005a9fd20572c412aee9bc2cf043cf8d6d3f
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-01-21 11:01:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (43.94%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int[] sum = new int[n];
+        
+        sum[0] = nums[0];
+        sum[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2;i<n;i++){
+            sum[i] = Math.max(sum[i-1], sum[i-2]+nums[i]);
+        }
+        
+        return sum[n-1];
+    }
+}
